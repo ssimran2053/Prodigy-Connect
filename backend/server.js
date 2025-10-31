849ba2285173dcb9918a4e8ab2d3ca2d4422a4eb
Simranpal Singh <simranpalsingh@Simranpals-MacBook-Pro.local>
2025-10-30 18:47:41 -0700

    chore: change server to localhost:5001 in backend server.js and add config/db.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 const app = express();
-app.get("/",(req, res) => {
-    res.send("Server is now ready");
-});
+app.get("/products",(req, res) => {});
 app.listen(5001, () => {
     console.log("Server started at http://localhost:5001");
 });
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connectDB } from "./config/db.js"; // make sure it is db.js
+connectDB();
